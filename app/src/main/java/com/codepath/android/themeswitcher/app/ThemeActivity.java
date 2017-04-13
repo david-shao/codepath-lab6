c5d94a2e868865361697b2aa2798f76612347971
David Shao <david.zeratul53@gmail.com>
2017-04-12 20:00:52 -0700

    fireeee

--- app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
@@ -1,9 +1,12 @@
 package com.codepath.android.themeswitcher.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 
 public class ThemeActivity extends ActionBarActivity {
@@ -11,11 +14,35 @@ public class ThemeActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // MUST BE SET BEFORE setContentView
+        Utils.onActivityCreateSetTheme(this);
+
         setContentView(R.layout.activity_theme);
+
+        setupSpinnerItemSelection();
     }
 
+    private void setupSpinnerItemSelection() {
+        Spinner spThemes = (Spinner) findViewById(R.id.spThemes);
+        spThemes.setSelection(ThemeApplication.currentThemeIndex);
+        ThemeApplication.currentThemeIndex = spThemes.getSelectedItemPosition();
 
-    @Override
+        spThemes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (ThemeApplication.currentThemeIndex != position) {
+                    Utils.changeToTheme(ThemeActivity.this, position);
+                }
+                ThemeApplication.currentThemeIndex = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_theme, menu);
---
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeApplication.java
@@ -0,0 +1,12 @@
+package com.codepath.android.themeswitcher.app;
+
+import android.app.Application;
+
+/**
+ * Created by David on 4/12/17.
+ */
+
+public class ThemeApplication extends Application {
+    // App level variable to retain selected spinner value
+    public static int currentThemeIndex;
+}
